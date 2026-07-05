daed22ccd6d435b77e13dc7fce5e7d2cf0c2f8e9
Eli Taylor <116466727+elitay152@users.noreply.github.com>
2026-07-05 10:50:01 -0700

    Delete ignition/mmodules directory

--- ignition/mmodules/FederatedTrainingCoordinatorModule.ts
+++
@@ -1,6 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("FederatedTrainingCoordinatorModule", (m) => {
-  const coordinator = m.contract("FederatedTrainingCoordinator");
-  return { coordinator };
-});
